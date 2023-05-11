b34d688a72c8e89b26cce33ad5a0726df050f8f9
gueffieb <benjamin.gueffier@edu.esiee.fr>
2023-05-11 10:29:35 +0200

    Favicon / Font

--- quiz-ui/src/views/QuestionsManager.vue
+++ quiz-ui/src/views/QuestionsManager.vue
@@ -12,7 +12,7 @@ export default {
         let currentQuestion = {
             questionTitle: "Comment s'appelle l'âne dans Shrek ?",
             questionText: "Nom de l'âne, de Shrek : ",
-            possibleAnswers: ["L'âne","Damso","Frédéric","Michel"],
+            possibleAnswers: ["L'âne","Damso","Frédéric"],
         };
         let currentQuestionPosition = 0;
         let totalNumberOfQuestion = 1;
@@ -32,10 +32,9 @@ export default {
 </script>
 
 <template>
-    <h1>Question {{ currentQuestionPosition }} / {{ totalNumberOfQuestion }}</h1>
+    <h1 style="font-family:'ShrekFont';">Question {{ currentQuestionPosition }} / {{ totalNumberOfQuestion }}</h1>
     <QuestionDisplay :question="currentQuestion" @click-on-answer="answerClickedHandler" />
 </template>
 
 <style>
-
 </style>
